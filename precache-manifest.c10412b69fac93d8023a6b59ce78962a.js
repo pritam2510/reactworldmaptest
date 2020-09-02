b38d536c433993d8c43c82d79ab181193ad75225
pritam2510 <pritam.d95@gmail.com>
2020-09-02 13:08:14 +0530

    Updates

---
+++ precache-manifest.c10412b69fac93d8023a6b59ce78962a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110fc217864f7b0c9516d0356c7c7b95",
+    "url": "/reactworldmaptest/index.html"
+  },
+  {
+    "revision": "84d375d5aac138983f84",
+    "url": "/reactworldmaptest/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cf1161aee54e77982756",
+    "url": "/reactworldmaptest/static/css/main.2e461e59.chunk.css"
+  },
+  {
+    "revision": "84d375d5aac138983f84",
+    "url": "/reactworldmaptest/static/js/2.2e6e91b7.chunk.js"
+  },
+  {
+    "revision": "cf1161aee54e77982756",
+    "url": "/reactworldmaptest/static/js/main.70940630.chunk.js"
+  },
+  {
+    "revision": "ce771fc264be18c7df40",
+    "url": "/reactworldmaptest/static/js/runtime~main.52bb4769.js"
+  }
+]);
